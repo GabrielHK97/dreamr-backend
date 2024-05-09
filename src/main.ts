2388afdef7f0945b934ff94c9323f40e8a93eaf6
Gabriel Kubota <gabrielkhiroshi@gmail.com>
2024-05-09 19:34:04 -0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv'
-  
+import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const options = {
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
-    credentials: true,
   };
   app.enableCors(options);
-  await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
-
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
