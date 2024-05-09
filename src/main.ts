df23f2a7296c100110e4400109b60b0a4dda4f4c
Gabriel Kubota <gabrielkhiroshi@gmail.com>
2024-05-09 20:08:45 -0300

    cors again

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const options = {
-    origin: 'http://localhost:3000',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
