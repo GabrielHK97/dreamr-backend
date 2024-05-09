c18c64da88e1453dddd8da8fc332917792fe3a37
Gabriel Kubota <gabrielkhiroshi@gmail.com>
2024-05-09 19:58:21 -0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const options = {
     origin: '*',
+    allowedHeaders: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
